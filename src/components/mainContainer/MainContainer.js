10635c4f91bbec5cebe09440bce4cbe78e643942
harithamv <haritha.mvijaya@gmail.com>
2024-02-18 11:47:37 +0530

    added search functionality

--- src/components/mainContainer/MainContainer.js
+++ src/components/mainContainer/MainContainer.js
@@ -5,21 +5,40 @@ import CardContainer from "../cardContainer/CardContainer";
 const MainContainer = () => {
   const [resData, setResData] = useState([]);
   const [searchEle,setSearchEle]=useState("");
+  const [searchList,setSearchList]=useState([])
   const searchRestaurants=(e)=>{
     setSearchEle(e.target.value)
+    
+    const searchFilter=resData.filter((item)=>{
+      return item.info.name.toLowerCase().includes(e.target.value.toLowerCase())
+    })
+    
+    setSearchList(searchFilter)
+
+   
   }
 
   useEffect(() => {
     async function fetchData() {
-      const response = await fetch(
+      try{      const response = await fetch(
         "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9715987&lng=77.5945627&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
       );
       const cardData = await response.json();
+      
       setResData(
         cardData?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle
           ?.restaurants
       );
+      setSearchList(cardData?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle
+          ?.restaurants)
+      
     }
+    catch(err){
+      console.log(err.message)
+    }
+
+  }
+   
     fetchData();
   }, []);
   return (
@@ -29,7 +48,7 @@ const MainContainer = () => {
       onChange={(e)=>searchRestaurants(e)}/>
       </div>
       <div className="sub-container">
-        {resData.map((item) => {
+        {searchList.map((item) => {
           return <CardContainer key={item.info.id} resData={item.info} />;
         })}
       </div>
